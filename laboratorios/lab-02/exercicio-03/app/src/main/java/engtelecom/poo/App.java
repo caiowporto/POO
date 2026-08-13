be8c707d8e4ef05cab46089a9c421e208ebcf619
Caio Henrique Weber Porto <caio.hwp2005@aluno.ifsc.edu.br>
2026-08-13 13:22:48 -0300

    Parte 3 do exercício 3 finalizada.

--- laboratorios/lab-02/exercicio-03/app/src/main/java/engtelecom/poo/App.java
+++ laboratorios/lab-02/exercicio-03/app/src/main/java/engtelecom/poo/App.java
@@ -3,14 +3,39 @@ package engtelecom.poo;
 public class App {
     public static void main(String[] args) {
         if (args.length > 1) {
-            int tamanho = Integer.parseInt(args[1]);
-            if ("triangulo".equals(args[0])) {
-                for (int i = 1; i <= tamanho; i++) {
-                    for (int j = 0; j < i; j++){
+            switch (args[0]){
+                case "triangulo" -> {
+                    int tamanho = Integer.parseInt(args[1]);
+                    for (int i = 1; i <= tamanho; i++) {
+                        for (int j = 0; j < i; j++){
+                            IO.print("*");
+                        }
+                        IO.println();
+                    }
+                }
+                case "retangulo" -> {
+                    int largura = Integer.parseInt(args[1]);
+                    int tamanho = Integer.parseInt(args[2]);
+                    for(int i = 0; i < largura; i++){
+                        IO.print("*");
+                    }
+                    IO.println();
+                    for (int j = 1; j < (tamanho-1); j++){
+                        for (int k = 1; k <= largura; k++){
+                            if(k == 1 || k == largura) {
+                                IO.print("*");
+                            } else {
+                                IO.print(" ");
+                            }
+                        }
+                        IO.println();
+                    }
+                    for(int i = 0; i < largura; i++){
                         IO.print("*");
                     }
                     IO.println();
                 }
+                default -> IO.print("Erro.");
             }
         }
     }
