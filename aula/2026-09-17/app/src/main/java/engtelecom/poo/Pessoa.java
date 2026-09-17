a4b2f47ee20f91ca66f0721a5b9e8ab2434999ce
Caio Porto <caio.hwp2005@aluno.ifsc.edu.br>
2026-09-17 17:36:17 -0300

    Criando projeto aula 17/09

---
+++ aula/2026-09-17/app/src/main/java/engtelecom/poo/Pessoa.java
@@ -0,0 +1,46 @@
+package engtelecom.poo;
+
+public class Pessoa {
+    // atributos
+
+    private String nome;
+    private String email;
+    private final int id;
+    private static int CONTADOR = 0;
+
+    // métodos
+
+
+    public Pessoa(String nome, String email) {
+        this.email = email;
+        this.nome = nome;
+        this.id = ++CONTADOR;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+//    @Override
+//    public String toString() {
+//        return "Pessoa{" +
+//                "nome='" + nome + '\'' +
+//                ", email='" + email + '\'' +
+//                ", id=" + id +
+//                '}';
+//    }
+
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append("Nome: ").append(nome);
+        sb.append("\nEmail: ").append(email);
+        sb.append("\nId: ").append(id);
+        return sb.toString();
+    }
+}
